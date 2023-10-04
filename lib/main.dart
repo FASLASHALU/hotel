c3c403210d93b54ca91389a4220b650c92dd12ec
FASLA V M <141605898+FASLASHALU@users.noreply.github.com>
2023-10-04 11:06:46 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:hotel/home.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(home: page(),debugShowCheckedModeBanner: false,));
+}
